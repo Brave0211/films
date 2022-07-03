2193fb72be39be99d5d3b69cfaac8e6a2fe750a8
Brave0211 <abduqodirovjasurbek11@gmail.com>
2022-07-03 23:33:18 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,40 +1,48 @@
-const elList = document.querySelector('.js-list');
+const elList = document.querySelector(".js-list");
+const elSel = document.querySelector(".js-select");
+const elSelect = document.querySelector(".js-select2");
+const elInput = document.querySelector(".js-input");
 
 function domgaChiqarator(array, node) {
-   for(cinema of array) {
-      let newItem = document.createElement('li')
-      newItem.setAttribute("class", "item");
-      let newSpan = document.createElement('span')
-      let newTitle = document.createElement('h3')
-      let newImg = document.createElement('img')
-      newImg.setAttribute("class", "img");
-      let newDesc = document.createElement('p')
-      let newDate = document.createElement('span')
-      let newGen = document.createElement('p')
+  for (cinema of array) {
+    let newItem = document.createElement("li");
+    newItem.setAttribute("class", "item");
+    let newSpan = document.createElement("span");
+    let newTitle = document.createElement("h3");
+    let newImg = document.createElement("img");
+    newImg.setAttribute("class", "img");
+    let newDesc = document.createElement("p");
+    let newDate = document.createElement("span");
+    let newGen = document.createElement("p");
 
-      newSpan.textContent = `ID: ${cinema.id}`
-      newTitle.textContent = `TITLE: ${cinema.title}`
-      newImg.src = `${cinema.poster}`;
-      newImg.setAttribute("alt", "Template img");
-      newImg.setAttribute("width", "100%");
-      newImg.setAttribute("height", "300");
-      newDesc.textContent = `OVERVIEW: ${cinema.overview}`
-      newDate.textContent = `RELEASE_DATE: ${cinema.release_date}`
-      newGen.textContent = `GENRES: ${cinema.genres}`
-      
-      newItem.appendChild(newSpan)
-      newItem.appendChild(newTitle)
-      newItem.appendChild(newImg)
-      newItem.appendChild(newDesc)
-      newItem.appendChild(newDate)
-      newItem.appendChild(newGen)
-      node.appendChild(newItem)
-   }
+    newSpan.textContent = `ID: ${cinema.id}`;
+    newTitle.textContent = `TITLE: ${cinema.title}`;
+    newImg.src = `${cinema.poster}`;
+    newImg.setAttribute("alt", "Template img");
+    newImg.setAttribute("width", "100%");
+    newImg.setAttribute("height", "300");
+    newDesc.textContent = `OVERVIEW: ${cinema.overview}`;
+    newDate.textContent = `RELEASE_DATE: ${cinema.release_date}`;
+    newGen.textContent = `GENRES: ${cinema.genres}`;
+
+    newItem.appendChild(newSpan);
+    newItem.appendChild(newTitle);
+    newItem.appendChild(newImg);
+    newItem.appendChild(newDesc);
+    newItem.appendChild(newDate);
+    newItem.appendChild(newGen);
+    node.appendChild(newItem);
+  }
 }
 
-domgaChiqarator(films, elList)
+films.sort((a,b) => {
+  if(b.title > a.title) {
+    return -1
+  }
+})
 
-const elSel = document.querySelector(".js-select");
+domgaChiqarator(films, elList);
+
 let result = [];
 
 elSel.addEventListener("change", function () {
@@ -46,7 +54,7 @@ elSel.addEventListener("change", function () {
       result.push(film);
     }
   });
-  
+
   domgaChiqarator(result, elList);
 });
 
@@ -55,12 +63,36 @@ const optionsList = new Set();
 const optionList = [];
 
 films.forEach((element) => {
-	element.genres.forEach((el) => optionsList.add(el));
-})  
-	
+  element.genres.forEach((el) => optionsList.add(el));
+});
+
 optionsList.forEach((genres) => {
-	let newOption = document.createElement('option');
-	newOption.textContent = genres;
-	newOption.value = genres;
-	elSel.appendChild(newOption);
-})
+  let newOption = document.createElement("option");
+  newOption.textContent = genres;
+  newOption.value = genres;
+  elSel.appendChild(newOption);
+});
+
+elInput.addEventListener("change", function () {
+  elList.innerHTML = "";
+  result = [];
+  selVal1 = elInput.value;
+  films.forEach((film) => {
+    if (film.title.includes(selVal1)) {
+      result.push(film);
+    }
+  });
+
+  domgaChiqarator(result, elList);
+});
+elSelect.addEventListener("change", function () {
+  elList.innerHTML = "";
+  result = [];
+  selVal2 = elSelect.value;
+  result = films.sort((a, b) => {
+    if (b.title > a.title) {
+      return -1;
+    }
+  });
+  domgaChiqarator(result, elList);
+});
